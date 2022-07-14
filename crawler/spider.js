16fb776e43bf6e32f188265863b4922edddfcbac
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-14 02:08:09 -0300

    little change

--- crawler/spider.js
+++ crawler/spider.js
@@ -4,28 +4,18 @@ const axios = require("axios");
 
 const url = "https://maisesports.com.br/agenda/proximas/lol/";
 
-axios(url).then((response) => {
-  const html = response.data;
-  const $ = cheerio.load(html);
-  $(".sc-135bb3bf-2 sc-135bb3bf-3 gwKKIh bZUIzg", html).each(function () {
-    console.log("text");
-  });
-});
-
-// request(
-//   "https://maisesports.com.br/agenda/proximas/lol/",
-//   function (err, res, body) {
-//     if (err) {
-//       console.log("ERRO: " + err);
-//     } else {
-//       var $ = cheerio.load(body);
-//       // TIMES DA LIGA CBLOL
-//       $("a").each(function () {
-//         var team = $(this).find(
-//           ".sc-135bb3bf-2 sc-135bb3bf-3 gwKKIh bZUIzg span"
-//         );
-//         console.log(team);
-//       });
-//     }
-//   }
-//);
+request(
+  "https://maisesports.com.br/agenda/proximas/lol/",
+  function (err, res, body) {
+    if (err) {
+      console.log("ERRO: " + err);
+    } else {
+      var $ = cheerio.load(body);
+      // TIMES DA LIGA CBLOL
+      $("a").each(function () {
+        var team = $(this).text().trim();
+        console.log("jogo");
+      });
+    }
+  }
+);
