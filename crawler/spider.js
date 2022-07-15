8db3bb93d3b7b0a08ad0105876178b414b776e65
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-14 23:26:01 -0300

    Times do cblol

--- crawler/spider.js
+++ crawler/spider.js
@@ -4,18 +4,18 @@ const axios = require("axios");
 
 const url = "https://maisesports.com.br/agenda/proximas/lol/";
 
-request(
-  "https://maisesports.com.br/agenda/proximas/lol/",
-  function (err, res, body) {
-    if (err) {
-      console.log("ERRO: " + err);
-    } else {
-      var $ = cheerio.load(body);
-      // TIMES DA LIGA CBLOL
-      $("a").each(function () {
-        var team = $(this).text().trim();
-        console.log("jogo");
-      });
-    }
+request({ url, gzip: true }, function (err, res, body) {
+  if (err) {
+    console.log("ERRO: " + err);
+  } else {
+    var $ = cheerio.load(body);
+    // TIMES DA LIGA CBLOL
+    $("a.jCVTeL").each(function () {
+      var game = $(this).find("div.mobileTeamContainer > p").text().trim();
+      var liga = $(this).find("div.gwKKIh").text().trim();
+      if (liga.includes("CBLOL:")) {
+        console.log(game);
+      }
+    });
   }
-);
+});
