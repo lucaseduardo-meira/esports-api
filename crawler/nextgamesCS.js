108ff3637d1b9cc50a3294172a7d98a97301784d
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-15 23:59:46 -0300

    Creation nextgamesCS

---
+++ crawler/nextgamesCS.js
@@ -0,0 +1,46 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+const url = "https://maisesports.com.br/agenda/proximas/csgo/";
+
+request({ url, gzip: true }, function (err, res, body) {
+  if (err) {
+    console.log("ERRO: " + err);
+  } else {
+    var $ = cheerio.load(body);
+    var ligas = [];
+    // Coloca todas as ligas que terão jogos na array ligas
+    $("a.iLMKBR > div.gwKKIh > div").each(function () {
+      const indice = $(this).text().indexOf(":");
+      const liga = $(this).text().slice(0, indice);
+      if (!ligas.includes(liga)) {
+        ligas.push(liga);
+      }
+    });
+    // Ordena os jogos de cada liga com data
+    ligas.forEach((liga) => {
+      console.log(liga);
+      $("a.iLMKBR").each(function () {
+        // Informações de liga e horario do jogo
+        const info_jogo = $(this).find("div.gwKKIh").text().trim();
+        var liga_jogo = info_jogo.slice(0, info_jogo.indexOf(":"));
+        // R0ecebe os jogos dos times que fazem parte da liga que está sendo procurada
+        if (liga_jogo.includes(liga)) {
+          var time1 = $(this)
+            .find("div.mobileTeamContainer > p")
+            .first()
+            .text()
+            .trim();
+          var time2 = $(this)
+            .find("div.mobileTeamContainer > p")
+            .last()
+            .text()
+            .trim();
+          var data = $(this).find("div.gwKKIh span").text().trim();
+          console.log(`=======>${time1} X ${time2} => ${data}`);
+        }
+      });
+    });
+  }
+});
