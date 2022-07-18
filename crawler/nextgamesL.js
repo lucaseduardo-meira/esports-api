b0f0cfcb00ba90eca851fb4c0915772f475f5be9
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-18 01:19:45 -0300

    Criação dos json e aplicação

--- crawler/nextgamesL.js
+++ crawler/nextgamesL.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const axios = require("axios");
+const fs = require("fs");
 
 const url = "https://maisesports.com.br/agenda/proximas/lol/";
 
@@ -10,6 +11,7 @@ request({ url, gzip: true }, function (err, res, body) {
   } else {
     var $ = cheerio.load(body);
     var ligas = [];
+    var lol = [];
     // Coloca todas as ligas que terão jogos na array ligas
     $("a.jCVTeL > div.gwKKIh > div").each(function () {
       const indice = $(this).text().indexOf(":");
@@ -23,28 +25,46 @@ request({ url, gzip: true }, function (err, res, body) {
       console.log("Não há jogos para listar");
     } else {
       ligas.forEach((liga) => {
-        console.log(liga);
+        var campeonato = {};
+        campeonato.nome = liga;
+        var matches = [];
         $("a.jCVTeL").each(function () {
           // Informações de liga e horario do jogo
           const info_jogo = $(this).find("div.gwKKIh").text().trim();
           var liga_jogo = info_jogo.slice(0, info_jogo.indexOf(":"));
+          let match = {};
           // Recebe os jogos dos times que fazem parte da liga que está sendo procurada
-          if (liga_jogo.includes(liga)) {
-            var time1 = $(this)
+          if (liga_jogo === liga) {
+            match.time1 = $(this)
               .find("div.mobileTeamContainer > p")
               .first()
               .text()
               .trim();
-            var time2 = $(this)
+            match.time2 = $(this)
               .find("div.mobileTeamContainer > p")
               .last()
               .text()
               .trim();
-            var data = $(this).find("div.gwKKIh span").text().trim();
-            console.log(`=======>${time1} X ${time2} => ${data}`);
+            match.data = $(this).find("div.gwKKIh span").text().trim();
+            matches.push(match);
           }
         });
+        campeonato.matches = matches;
+        lol.push(campeonato);
       });
+      fs.writeFile(
+        "./League of legends/nextgameslol.json",
+        JSON.stringify(lol, null, "  "),
+        "utf-8",
+        (error, result) => {
+          if (error) {
+            console.error(error);
+            return;
+          }
+
+          console.log(result);
+        }
+      );
     }
   }
 });
