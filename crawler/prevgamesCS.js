6765d52ca8c504db63bc47e50872f75372ccbde1
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-19 00:58:09 -0300

    Update prevgamesCS.js

--- crawler/prevgamesCS.js
+++ crawler/prevgamesCS.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const axios = require("axios");
+const fs = require("fs");
 
 const url = "https://maisesports.com.br/agenda/antigas/csgo/";
 
@@ -10,6 +11,7 @@ request({ url, gzip: true }, function (err, res, body) {
   } else {
     var $ = cheerio.load(body);
     var ligas = [];
+    var cs = [];
     // Coloca todas as ligas que terão jogos na array ligas
     $("a.iLMKBR > div.gwKKIh > div").each(function () {
       const indice = $(this).text().indexOf(":");
@@ -19,42 +21,46 @@ request({ url, gzip: true }, function (err, res, body) {
       }
     });
     // Ordena os jogos de cada liga com data
-    ligas.forEach((liga) => {
-      console.log(liga);
-      $("a.iLMKBR").each(function () {
-        // Informações de liga e horario do jogo
-        const info_jogo = $(this).find("div.gwKKIh").text().trim();
-        var liga_jogo = info_jogo.slice(0, info_jogo.indexOf(":"));
-        // R0ecebe os jogos dos times que fazem parte da liga que está sendo procurada
-        if (liga_jogo.includes(liga)) {
-          var time1 = $(this)
-            .find("div.mobileTeamContainer > p ")
-            .first()
-            .text()
-            .trim();
-          var result1 = $(this)
-            .find("div.mobileTeamContainer > div.exlLqx ")
-            .first()
-            .text()
-            .trim();
-          var time2 = $(this)
-            .find("div.mobileTeamContainer > p")
-            .last()
-            .text()
-            .trim();
-          var result2 = $(this)
-            .find("div.mobileTeamContainer > div.exlLqx")
-            .last()
-            .text()
-            .trim();
-          var data = $(this).find("div.gwKKIh span").text().trim();
-          console.log(
-            `=======>${time1 + " " + result1}  X  ${
-              result2 + " " + time2
-            } => ${data}`
-          );
-        }
+    if (ligas.length == 0) {
+      console.log("Não há jogos para listar");
+    } else {
+      ligas.forEach((liga) => {
+        console.log(liga);
+        $("a.iLMKBR").each(function () {
+          // Informações de liga e horario do jogo
+          const info_jogo = $(this).find("div.gwKKIh").text().trim();
+          var liga_jogo = info_jogo.slice(0, info_jogo.indexOf(":"));
+          // R0ecebe os jogos dos times que fazem parte da liga que está sendo procurada
+          if (liga_jogo.includes(liga)) {
+            var time1 = $(this)
+              .find("div.mobileTeamContainer > p ")
+              .first()
+              .text()
+              .trim();
+            var result1 = $(this)
+              .find("div.mobileTeamContainer > div.exlLqx ")
+              .first()
+              .text()
+              .trim();
+            var time2 = $(this)
+              .find("div.mobileTeamContainer > p")
+              .last()
+              .text()
+              .trim();
+            var result2 = $(this)
+              .find("div.mobileTeamContainer > div.exlLqx")
+              .last()
+              .text()
+              .trim();
+            var data = $(this).find("div.gwKKIh span").text().trim();
+            console.log(
+              `=======>${time1 + " " + result1}  X  ${
+                result2 + " " + time2
+              } => ${data}`
+            );
+          }
+        });
       });
-    });
+    }
   }
 });
